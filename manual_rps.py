364a318548aecd3667fae2a12100c4a0a64f8d27
Jasmine Kaur <jasminekaur@Jasmines-MBP.lan>
2023-02-10 15:24:32 +0000

    Creating get winner funtion which will decide who wins

--- manual_rps.py
+++ manual_rps.py
@@ -9,6 +9,26 @@ def get_user_choice():
     userInput = input("Enter your choice")
     return userInput
 
-get_computer_choice()
-get_user_choice()
+def get_winner(cc, userInput):
+    if cc == userInput:
+        print("Tie")
+    elif userInput == "Rock":
+        if cc == "Paper":
+            print("You Lost")
+        else:
+            print("You Won!")
+    elif userInput == "Paper":
+        if cc == "rock":
+                print("You Won")
+        else:
+                print("You Lost")
+    elif userInput == "Scissors":
+        if cc == "Rock":
+            print("You Lost")
+        else:
+            print("You Won!")
+    else:
+        print("Error")
+
+get_winner("Rock", "Sissors")
 # %%
