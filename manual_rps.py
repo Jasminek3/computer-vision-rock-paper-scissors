ec4c712f6f58d48c57e5ca4a3973624df033a747
Jasmine Kaur <jasminekaur@Jasmines-MacBook-Pro.local>
2023-02-11 22:38:56 +0000

    Changing print to return

--- manual_rps.py
+++ manual_rps.py
@@ -3,7 +3,7 @@ import random
 
 def get_computer_choice():
     computer_choice = random.choice(["Rock", "Paper", "Scissors"])
-    return computer_choice
+    return print(computer_choice)
 
 def get_user_choice():
     user_choice = input("Enter your choice")
@@ -11,25 +11,20 @@ def get_user_choice():
 
 def get_winner(computer_choice, user_choice):
     if computer_choice == "Rock" and user_choice == "Paper":
-        print("You Won!")
-        return "Player"
+        return "You Won!"
     elif computer_choice == "Rock" and user_choice == "Scissors":
-        print("You Lost")
-        return "Cpu"
+        return "You Lost"
     elif computer_choice == "Paper" and user_choice == "Scissors":
-        print("You Won!")
-        return "Player"
+        return "You Won!"
     elif computer_choice == "Paper" and user_choice == "Rock":
-        print("You Lost")
-        return "Cpu"
+        return "You Lost"
     elif computer_choice == "Scissors" and user_choice == "Rock":
-        print("You Won!")
-        return "Player"
+        return "You Won!"
     elif computer_choice == "Scissors" and user_choice == "Paper":
-        print("You Lost")
-        return "Cpu"
+        return "You Lost"
     else:
         print("It is a tie!")
-        return "Tie"
+        return "It is a tie!"
 
+get_winner()
 # %%
