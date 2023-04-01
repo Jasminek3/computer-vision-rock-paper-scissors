b2b82819e3798ed970431d2a60779acea211b907
Jasmine Kaur <jasminekaur@Jasmines-MBP.lan>
2023-04-01 18:07:52 +0100

    Adding the countdown

--- camera_rps.py
+++ camera_rps.py
@@ -1,12 +1,15 @@
 #%%
 import cv2
 from keras.models import load_model
-import numpy as np
+import numpy as np 
+import time
 
 def get_prediction():
     model = load_model('keras_model.h5')
     cap = cv2.VideoCapture(0)
     data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+    rtnVal = np.argmax(model)
+    rtnVal
 
     while True: 
         ret, frame = cap.read()
@@ -20,9 +23,20 @@ def get_prediction():
         print(prediction)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-                
+            
     # After the loop release the cap object
     cap.release()
     # Destroy all the windows
     cv2.destroyAllWindows()
+
+def countdown(time_sec):
+    while time_sec:
+        mins, secs = divmod(time_sec, 60)
+        timeformat = '{:02d}:{:02d}'.format(mins, secs)
+        print(timeformat, end='\r')
+        time.sleep(1)
+        time_sec -= 1
+
+countdown(5)
+
 # %%
